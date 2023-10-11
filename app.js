17bba037cdd4aa070f3aba0441a0b4a5c1c82858
AarnoVeitola <121627348+AarnoVeitola@users.noreply.github.com>
2023-10-11 09:33:54 +0300

    assignment

--- app.js
+++ app.js
@@ -10,17 +10,19 @@ const responseDetails = {
 };
 
 const data = {
-  count: 0,
+  visits: 0,
 };
 
 const handleRequest = async (request) => {
   const url = new URL(request.url);
-  if (url.pathname === "/count") {
-    data.count++;
+  if (url.pathname === "/visits") {
+    data.visits++;
     return new Response(await renderFile("count.eta", data), responseDetails);
+  } else if (url.pathname === "/meaning") {
+    return new Response("Seeking truths beyond meaning of life, you will find 43.");
+  } else {
+    return new Response("Nothing here yet.");
   }
-
-  return new Response("Hello you!");
 };
 
 serve(handleRequest, { port: 7777 });
